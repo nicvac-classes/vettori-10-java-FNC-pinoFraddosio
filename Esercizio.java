24843351d73469e87f3f0445f2da279c01e37ce0
FNC-pinoFraddosio <benedetto.fraddosio@marconibari.edu.it>
2024-11-29 10:20:20 +0000

    Consegna

--- Esercizio.java
+++ Esercizio.java
@@ -6,20 +6,43 @@ import java.util.Scanner;
 // Classe principale, con metodo main
 class Esercizio {
     // Il programma parte con una chiamata a main().
-    public static void main(String args[])
-    {
-        //Variabili del programma
-        String nome;
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
 
-        //Creo l'oggetto in per l'input da tastiera
-        Scanner in = new Scanner( System.in );
+        int n;
 
-        //Leggo l'input da tastiera
-        System.out.print("Inserisci il tuo nome: ");
-        nome = in.nextLine();
+        System.out.print("Inserire il numero degli studenti: ");
+        n = in.nextInt();
 
-        //Output del nome acquisito da tastiera
-        System.out.println("Ciao "+nome+"!");
+        String[] nomi = new String[n];
+        float[] voti = new float[n];
+        int[] crediti = new int[n];
+
+        for(int i = 0; i < n; i++){
+            System.out.print("Inserire il nome del " + (i+1) + "° Studente: ");
+            nomi[i] = in.next();
+            System.out.print("Inserire il voto di " + nomi[i] + ": ");
+            voti[i] = in.nextFloat();
+            System.out.print("Inserire i crediti formativi di " + nomi[i] + ": " );
+            crediti[i] = in.nextInt();
+        }
+
+        float media = 0;
+        
+        for(int i = 0; i < n; i++){
+            media += voti[i];
+        }
+
+        media /= n;
+
+        System.out.println("La media delle votazioni degli studenti sono: " + media);
+
+
+        for(int i = 0; i < n; i++){
+            if(voti[i] > media && crediti[i] >= 6){
+                System.out.println(nomi[i] + " ha la votazione più alta di " + media + " e i crediti formativi maggiori o uguali a 6");
+            }
+        }
     }
 }
 
